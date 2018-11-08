4093320200fc68a8c2410c126253ed1ac9c8df69
PoChin Cheng <pochin.chng@pcc.edu>
2018-11-08 01:31:56 -0800

    find_all getting empty list with region-content-header...  this need to be resolved

--- program.py
+++ program.py
@@ -1,11 +1,16 @@
+import bs4
+import requests
+
+
 
 def main():
     print_the_header()
 
     code = input('What zipcode do you want the weather for (97201)? ')
 
-    # get html from web
-    get_html_from_web(code)
+    html = get_html_from_web(code)
+
+    get_weather_from_html(html)
     # parse the html
     # display for the forecast
 
@@ -21,7 +26,21 @@ def print_the_header():
 def get_html_from_web(zipcode):
     url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
     # print(url)
-    requests(url)
+    response = requests.get(url)
+    # print(response.status_code)
+    # print(response.text[0:250])
+    return response.text
+
+def get_weather_from_html(html):
+    # cityCss = '.region-content-header h1'
+    # weatherScaleCss = '.wu-unit-temperature .wu-label'
+    # weatherTempCss = '.wu-unit-temperature .wu-value'
+    # weatherConditionCss = '.condition-icon'
+
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+    loc = soup.find_all("div", class_='region-content-header')
+    print(loc)
+
 
 if __name__ == '__main__':
     main()
